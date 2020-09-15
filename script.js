70eb759a00155124de11924dff4830dd9c2c80c0
Daniel DiCocco <dicoccod@bc.edu>
2020-09-14 23:21:11 -0400

    added attractions.json and new CSS changes

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 // TODO: load the dataset 
+let attractions;
+fetch('attractions.json')
+  .then(response => response.json())
+  .then(data => {
+		attractions = data;
+        attractions.sort((a, b) => b.Visitors - a.Visitors);  //sort desc
+    });
+    
+
 
 function filterData(category) {
-
-	/* **************************************************
+    /* **************************************************
 	 *
 	 * TODO: filter attractions by the selected category
 	 * TODO: filter top 5 attractions
@@ -16,7 +24,23 @@ function filterData(category) {
 	 *
 	 * **************************************************/
 
+    let filteredAttractions = attractions.filter(function (el) {
+        return el.Category === category;
+    });
+    console.log(filteredAttractions);
+
+    let top5 = filteredAttractions.slice(0,5)
+    console.log(top5);  //debugging
+    renderBarChart(top5);
+     
 }
 
 // TODO: Define an event listener for the dropdown menu
 //       Call filterData with the selected category
+
+let selectElem = document.querySelector('#attraction-category');
+selectElem.addEventListener('change', (event) => {
+    let category = event.target.value;
+    console.log(category);  //debugging
+    filterData(category);
+  });
